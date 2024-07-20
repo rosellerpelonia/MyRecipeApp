38a083d2903eec5d250d4eba2913349236249d18
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-07-20 20:13:44 +0800

    setup gradle for lifecycle and network callbacks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,15 @@ android {
 
 dependencies {
 
+    //Compose ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
+
+    //Network calls
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    //Image loading
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
