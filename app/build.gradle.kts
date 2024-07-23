97af38bdbbddbba712bf31edba0eae2752aef5ed
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-07-23 15:46:19 +0800

    Category Item setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
 
     //Network calls
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    //Json to Kotlin object mapping
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     //Image loading
